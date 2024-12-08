68c6df94d405fb2fba20d8e10f3841f12b1bd090
aishaa741 uyyu <mahmoud.aisha@iisgalvanimi.edu.it>
2024-12-08 20:17:13 +0000

    punto 4

--- mahmoud-aisha-fila-b/src/app/app.component.ts
+++ mahmoud-aisha-fila-b/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
+import { FifthComponent } from './fifth/fifth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecondComponent, ThirdComponent, FourthComponent, FifthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
